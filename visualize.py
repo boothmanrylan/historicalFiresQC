f504477f5b042ecc5e6791f00d701bc7e074be04
Rylan Boothman <boothmanrylan@gmail.com>
2020-10-30 14:41:15 -0400

    fix import

--- visualize.py
+++ visualize.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import ListedColormap
 
 colours = [
     (0, 0,    0), # white for clouds
