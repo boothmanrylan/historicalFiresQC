4e93258a7f707800d81764153842570eadfd2000
Rylan Boothman <boothmanrylan@gmail.com>
2021-11-09 12:42:45 -0500

    update colours to be more appealing

--- visualize.py
+++ visualize.py
@@ -7,9 +7,9 @@ MAX_ANNOT = 5
 
 COLOURS = [
     (0.5, 0.5, 0.5),  # grey for no data
-    (0.5, 0.25, 0),   # brown for land (non-burnt)
+    (0.3, 0.15, 0),   # brown for land (non-burnt)
     (1, 0, 0),        # red for burns
-    (0, 1, 0),        # blue for water
+    (0, 0, 1),        # blue for water
     (0, 0, 0),        # black for shadow
     (1, 1, 1),        # white for clouds
 ]
